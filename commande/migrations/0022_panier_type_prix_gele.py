8db8d6b19f3997089f301e8ef08aa05c4c175902
MPIGA-ODOUMBA JESSE <86076146+mpigajesse@users.noreply.github.com>
2025-10-21 19:14:09 +0100

    fix: Corriger la dépendance de migration 0022

--- commande/migrations/0022_panier_type_prix_gele.py
+++ commande/migrations/0022_panier_type_prix_gele.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('commande', '0021_commande_date_paiement'),
+        ('commande', '0001_initial'),
     ]
 
     operations = [
